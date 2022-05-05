a30b0c24a887cf400f197f0811658f0e9fe75aaa
BryanRSW20 <87527079+BryanRSW20@users.noreply.github.com>
2022-05-05 14:17:56 -0300

    .

---
+++ src/main/java/br/senai/controller/FuncionarioController.java
@@ -0,0 +1,51 @@
+package br.senai.controller;
+
+import br.senai.model.Funcionario;
+import br.senai.repository.FuncionarioRepository;
+import br.senai.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Controller
+public class FuncionarioController {
+
+    @Autowired
+    FuncionarioService funcionarioService;
+
+    @GetMapping("/funcionario/list")
+    public String findAll(Model model) {
+        model.addAttribute("funcionario3a", funcionarioService.findAll());
+        return "funcionario/list";
+    }
+
+    @GetMapping("/funcionario/add")
+    public String add(Model model){
+        model.addAttribute("funcionario", new Funcionario());
+        return "funcionario/add";
+    }
+
+    @PostMapping("/funcionario/save")
+    public String save(Funcionario funcionario, Model model){
+        try{
+            funcionarioService.save(funcionario);
+            model.addAttribute("funcionario", funcionario);
+            model.addAttribute("isSaved", true);
+            return "funcionario/add";
+        } catch (Exception e){
+            model.addAttribute("funcionario", funcionario);
+            model.addAttribute("isError", true);
+            model.addAttribute("error", e.getMessage());
+            return "funcionario/add";
+        }
+    }
+
+
+
+}
---
+++ src/main/java/br/senai/service/FuncionarioServiceImpl.java
@@ -0,0 +1,34 @@
+package br.senai.service;
+
+import br.senai.model.Funcionario;
+import br.senai.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+
+    @Autowired
+    FuncionarioRepository funcionarioRepository;
+
+    @Override
+    public List< Funcionario > findAll() {
+        return funcionarioRepository.findAll();
+    }
+
+    @Override
+    public Funcionario findByNome(String nome) {
+        return null;
+    }
+
+    @Override
+    public Funcionario save(Funcionario funcionario){
+        try{
+            return funcionarioRepository.save(funcionario);
+        } catch (Exception e){
+            throw e;
+        }
+    }
+}
